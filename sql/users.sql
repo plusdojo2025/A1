635a618ddd93885c3ba34414be0ef423bb4d5656
plusdojo2025-haijima <haijima-manami-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 11:58:12 +0900

    Users更新しました。

--- sql/users.sql
+++ sql/users.sql
@@ -8,3 +8,8 @@ FOREIGN KEY (live) REFERENCES prefectures (prefecture_id)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE
 );	
+
+INSERT INTO users(user_id,password,name,live) VALUES
+('onigawara','kyunmoe32','もえもえ',4),
+('zinguuzi','Maaay2025','まいやん',14),
+('kuro','Nagaoka25','いっちー',13);
