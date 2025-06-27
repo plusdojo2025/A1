ccae2cea12874adf26f50ca2ac6f00179e0dfacc
plusdojo2025-haijima <haijima-manami-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-27 11:27:19 +0900

    sql更新しました。
    dojouser4のデータをall.sqlに入れました。

--- sql/holds.sql
+++ sql/holds.sql
@@ -26,7 +26,27 @@ date_acquisition)VALUES
 -- dojouser3 中部制覇
 (4,4,'dojouser3','2025-06-15'),
 -- dojouser3 四国制覇
-(5,6,'dojouser3','2025-01-13');
+(5,7,'dojouser3','2025-01-13'),
+-- dojouser4 北海道制覇
+(6,1,'dojouser4','2025-05-07'),
+-- dojouser4 東北制覇
+(7,2,'dojouser4','2019-10-17'),
+-- dojouser4 関東制覇
+(8,3,'dojouser4','2020-11-11'),
+-- dojouser4 中部制覇
+(9,4,'dojouser4','2021-12-20'),
+-- dojouser4 近畿制覇
+(10,5,'dojouser4','2022-10-04'),
+-- dojouser4 中国制覇
+(11,6,'dojouser4','2023-06-08'),
+-- dojouser4 四国制覇
+(12,7,'dojouser4','2023-11-01'),
+-- dojouser4 九州(沖縄含む
+(13,8,'dojouser4','2024-10-21'),
+-- dojouser4 都道府県制覇
+(14,9,'dojouser4','2025-02-06');
+
+
 
 /*holdsテーブルのデータを確認*/
 SELECT*FROM holds;
